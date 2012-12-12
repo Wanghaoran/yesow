2eb76435db79961804b355c7e0f0acff93127a4d
Wanghaoran <whrlmc@139.com>
2012-12-12 18:36:24 +0800

    更改数据库密码 Signed-off-by: Wanghaoran <whrlmc@139.com>

--- Public/config.inc.php
+++ Public/config.inc.php
@@ -20,7 +20,7 @@ return array(
   'DB_HOST' => 'localhost',  //服务器地址
   'DB_NAME' => 'yesow',  //数据库名
   'DB_USER' => 'root',  //用户名
-  'DB_PWD' => '',  //密码
+  'DB_PWD' => 'jilexingqiu',  //密码
   'DB_PORT' => '3306',  //端口
   'DB_PREFIX' => 'yesow_',  //数据库表前缀
 
